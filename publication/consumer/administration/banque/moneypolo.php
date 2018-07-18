c7d7b2c52ff3ad8fd2debfb69dca6a0e0dc4199b
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2018-07-19 01:30:29 +0200

    replace okpay scan & update moneypolo

---
+++ publication/consumer/administration/banque/moneypolo.php
@@ -0,0 +1,167 @@
+[img src="https://business.livingxworld.com/images/logo/administration/banque/moneypolo.svg" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="moneypolo" /]
+<hr id="system-readmore" />
+[h2 class="text-center"][begins tags="strong" /]Presentation[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
+		Envoyer de l'argent à l'étranger ou via plus de 400 000 agences partenaires dans le monde entier. Plusieurs fois moins cher que les banques et autres services de transfert d'argent.
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[youtube source="D5Ki1Codvno" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Information[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[h3]coordinated[/h3]
+		[fa name="external-link" zoom="2x" /] [a href="https://moneypolo.com" target="_blank" rel="dofollow"]Website[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 2256/1, Archeologická, 150 04 Praha 5, Tchéquie [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+420246093204" rel="nofollow"]+420 246 093 204[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]6721866[/a] [br /]
+		[h3]Opening schedules[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /]Monday to Friday: --[ends tags="li" /]
+			[begins tags="li" /]Saturday: --[ends tags="li" /]
+			[begins tags="li" /]Sunday: --[ends tags="li" /]
+			[begins tags="li" /]holidays: --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Transport[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /][fa name="bus" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="subway" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="train" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="car" zoom="2x" /]  --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Others[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /][fa name="wheelchair" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="broadcast-tower" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="wifi" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="money-bill" zoom="2x" /] --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]These software and Apps[/h3]
+		[begins tags="ul" /]
+			[begins tags="li" /][a href="https://itunes.apple.com/us/developer/moneypolo/id1286658697?mt=8" target="_blank"][fa bs="fab" name="apple" zoom="2x" /] Apple[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="windows" zoom="2x" /] Microsoft[/a][ends tags="li" /]
+			[begins tags="li" /][a href="https://play.google.com/store/apps/details?id=com.mayzusfs.android.moneypolo&hl=fr" target="_blank"][fa bs="fab" name="android" zoom="2x" /] Android[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="linux" zoom="2x" /] Linux[/a][ends tags="li" /]
+		[ends tags="ul" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[h3]MAP[/h3]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2562.25448018767!2d14.329370515716727!3d50.04406397942138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b94c1da3f8cc7%3A0x6e034fa00935cfac!2sMoneyPolo!5e0!3m2!1sfr!2sfr!4v1531925194421" /]
+		[h4]Are you interested? [begins tags="small"]Click on the affiliate banner[ends="small" /][/h4] [br /]
+		
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+[none]
+[h2 class="text-center"][begins tags="strong" /]These photo[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt=""" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt=""" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt=""" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+[/none]
+
+[h2 class="text-center"][begins tags="strong" /]Automatic scanner[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]owner site[/h3]
+		[whois-domains url="moneypolo.com" /] 
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]HTTPS: SSL/TLS[/h3]
+		[ssllabs domains="moneypolo.com" /]
+		[h3]Your data are[/h3]
+		[api-myipms query="moneypolo.com" /]	
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]The Prices Proposed in Taxe (without promotion)[ends tags="strong" /] [/h2]
+--
+
+
+[h2 class="text-center"][begins tags="strong" /]Note and advice with requirement[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Badge[/h3]
+		[begins tags="div" class="card text-white bg-success" /]
+			[lxw-badge parent="administration" category="banque" entreprise="moneypolo" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-success" showembed="yes" /]
+			[begins tags="div" class="card-body" /]
+				[h5 class="card-title"]validé[/h5]
+			[ends tags="div" /]	
+		[ends tags="div" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Independent control[/h3]
+		[begins tags="blockquote" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Customer services[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Team atmosphere[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Web Regulations[ends tags="dt" /] 
+				[begins tags="dd" /] conforme[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Tester reviews[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Web security test[ends tags="dt" /] 
+				[begins tags="dd" /] DigiCert SHA2 Extended Validation Server CA : B (corrigé RC4 cipher)[ends tags="dd" /] 
+			[ends tags="dl" /]
+		[ends tags="blockquote" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Tell a Friends[ends tags="strong" /] [/h2]
+	[begins tags="div" class="row" /] 
+		[begins tags="div" class="col-md-12 center-block text-center" /]
+			[div class="addthis_inline_share_toolbox" /] 
+		[ends tags="div" /] 
+	[ends tags="div" /]
+ [br /] [br /]
+[h6]It is up to you to give your opinion: according to its customer service, its proposals, quality of the service and more.[/h6]
+
+
+[begins tags='script' more='type="application/ld+json"' /] 
+	{
+		"@context": "http://www.schema.org",
+		"@type": "LocalBusiness",
+		"name": "moneypolo",
+		"url": "https://moneypolo.com",
+		"logo": "https://business.livingxworld.com/images/logo/moneypolo.svg",
+		"telephone": "+420246093204",
+		"description": "Envoyer de l'argent à l'étranger ou via plus de 400 000 agences partenaires dans le monde entier. Plusieurs fois moins cher que les banques et autres services de transfert d'argent.",
+		"currenciesAccepted": "EUR",
+		"paymentAccepted": "card",
+		"address": {
+			"@type": "PostalAddress",
+			"streetAddress": "2256/1, Archeologická, 150 04 Praha 5, Tchéquie"
+		},
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2562.25448018767!2d14.329370515716727!3d50.04406397942138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b94c1da3f8cc7%3A0x6e034fa00935cfac!2sMoneyPolo!5e0!3m2!1sfr!2sfr!4v1531925194421"
+	}
+[ends tags='script' /]
