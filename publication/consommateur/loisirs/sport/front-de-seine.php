5c16d88499f1c87b9fb0466455a4e56a320b4d90
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2018-09-18 00:30:43 +0200

    scan & update front de seine

--- publication/consommateur/loisirs/sport/front-de-seine.php
+++ publication/consommateur/loisirs/sport/front-de-seine.php
@@ -1,13 +1,13 @@
-[img src="https://business.livingxworld.com/images/logo/loisirs/sport/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" /]
+[img src="https://business.livingxworld.com/images/logo/loisirs/sport/frontdeseine.png" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="front de seine" /]
 <hr id="system-readmore" />
 [h2 class="text-center"][begins tags="strong" /]Présentation[ends tags="strong" /] [/h2]
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
-		
+		Du sport. Du vrai.
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
-		[youtube source="" /]
+		[youtube source="AL44ukHbdPg" /]
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -16,21 +16,21 @@
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[h3]Coordonné[/h3]
-		[fa name="external-link" zoom="2x" /] [a href="https://" target="_blank" rel="dofollow"]Site internet[/a] [br /]
-		[fa name="map-marker-alt" zoom="2x" /] adresse [br /]
-		[fa name="phone" zoom="2x" /] [a href="tel:+" rel="nofollow"][/a] [br /]
-		SIRET/VAT [a href="https://" rel="nofollow"][/a] [br /]
+		[fa name="external-link" zoom="2x" /] [a href="https://frontdeseine.net" target="_blank" rel="dofollow"]Site internet[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 44 Rue Emeriau, 75015 Paris [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33145753537" rel="nofollow"]01 45 75 35 37[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]--[/a] [br /]
 		[h3]Horaires[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /]Lundi à Vendredi: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Samedi: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Dimanche: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Jour férié: xxhxx - xxhxx[ends tags="li" /]
+			[begins tags="li" /]Lundi à Vendredi: 6h45 - 23h30[ends tags="li" /]
+			[begins tags="li" /]Samedi: 9h00 - 20h30[ends tags="li" /]
+			[begins tags="li" /]Dimanche: 9h00 - 20h30[ends tags="li" /]
+			[begins tags="li" /]Jour férié: --[ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Transport[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /][fa name="bus" zoom="2x" /]  --[ends tags="li" /]
-			[begins tags="li" /][fa name="subway" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="bus" zoom="2x" /] Théâtre: 42 [ends tags="li" /]
+			[begins tags="li" /][fa name="subway" zoom="2x" /] Commerce: 8 / Dupleix: 6 / Charles Michels: 10[ends tags="li" /]
 			[begins tags="li" /][fa name="train" zoom="2x" /]  --[ends tags="li" /]
 			[begins tags="li" /][fa name="car" zoom="2x" /]  --[ends tags="li" /]
 		[ends tags="ul" /]	
@@ -51,7 +51,7 @@
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
 		[h3]La carte[/h3]
-		[gmaps url="" /]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4747926412724!2d2.283332315674047!3d48.84915597928655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e670051bcb9603%3A0x7b154ae8da0ea541!2sFront+de+Seine+-+Squash+%26+Fitness!5e0!3m2!1sfr!2sfr!4v1537218453616" /]
 		[h4]Êtes-vous intéresser? [begins tags="small"]Cliquer sur la bannière d'affiliation[ends="small" /][/h4] [br /]
 		
 	[ends tags="div" /]	
@@ -62,13 +62,13 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]Priotitaire du site[/h3]
-		[whois-domains url="" /] 
+		[whois-domains url="frontdeseine.net" /] 
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]HTTPS: SSL/TLS[/h3]
-		[ssllabs domains="" /]
+		[ssllabs domains="frontdeseine.net" /]
 		[h3]Vos données sont:[/h3]
-		[api-myipms query="" /]
+		[api-myipms query="frontdeseine.net" /]
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -102,26 +102,36 @@
 			[begins tags="table" class="table table-bordered" mdataprop="offers" mdatatype="http://schema.org/Offer" /] 
 				[begins tags="thead" /] 
 					[begins tags="tr" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
+						[begins tags="th" /]Type[ends tags="th" /] 
+						[begins tags="th" /]Initial[ends tags="th" /] 
+						[begins tags="th" /]Basic[ends tags="th" /] 
+						[begins tags="th" /]Avantage[ends tags="th" /] 
 					[ends tags="tr" /] 
 				[ends tags="thead" /] 
 				[begins tags="tbody" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-					[ends tags="tr" /]
-					[begins tags="tr" /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags="td" /]Squash[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="29"' /]€29.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
+						[begins tags='td' mdataprop='price' more='content="23"' /]€23.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="17"' /]€17.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
 					[ends tags="tr" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a] [ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
+						[begins tags="td" /]Training Gym[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="29"' /]€29.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
+						[begins tags='td' mdataprop='price' more='content="25"' /]€25.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="19"' /]€19.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /] 
+						[begins tags="td" /]Spinning[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="29"' /]€29.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
+						[begins tags='td' mdataprop='price' more='content="25"' /]€25.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="19"' /]€19.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /] 
+						[begins tags="td" /]--[ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.frontdeseine.net/acheter/" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.frontdeseine.net/acheter/" mdataprop="url"]acheter l'offre[/a] [ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.frontdeseine.net/acheter/" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
 					[ends tags="tr" /]
 				[ends tags="tbody" /] 
 			[ends tags="table" /] 
@@ -133,10 +143,10 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-6" /]
 		[h3]Badge[/h3]
-		[begins tags="div" class="card text-white bg-" /]
-			[lxw-badge parent="loisirs" category="sport" entreprise="" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-" showembed="yes" /]
+		[begins tags="div" class="card text-white bg-warning" /]
+			[lxw-badge parent="loisirs" category="sport" entreprise="front-de-seine" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-warning" showembed="yes" /]
 			[begins tags="div" class="card-body" /]
-				[h5 class="card-title"]advice final[/h5]
+				[h5 class="card-title"]Douteux[/h5]
 			[ends tags="div" /]	
 		[ends tags="div" /]		
 	[ends tags="div" /]	
@@ -153,7 +163,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Réglementation web[ends tags="dt" /] 
-				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+				[begins tags="dd" /] incomplet[ends tags="dd" /] 
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Avis testeur[ends tags="dt" /] 
@@ -161,7 +171,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Teste de sécurité web[ends tags="dt" /] 
-				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+				[begins tags="dd" /] Let's Encrypt Authority X3 : A[ends tags="dd" /] 
 			[ends tags="dl" /] 
 		[ends tags="blockquote" /]
 	[ends tags="div" /]	
@@ -180,27 +190,26 @@
 	{
 		"@context": "http://www.schema.org",
 		"@type": "LocalBusiness",
-		"name": "nom",
-		"url": "https://url.com",
-		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/",
-		"telephone": "+33100000000",
-		"description": "description",
+		"name": "front de seine",
+		"url": "https://frontdeseine.net",
+		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/frontdeseine.png",
+		"telephone": "+33145753537",
+		"description": "Du sport. Du vrai.",
 		"currenciesAccepted": "EUR",
 		"paymentAccepted": "cash, card",
 		"priceRange": "€€",
 		"address": {
 			"@type": "PostalAddress",
-			"streetAddress": "adresse",
-			"addressLocality": "Ville",
-			"addressRegion": "région",
-			"postalCode": "code postal",
-			"addressCountry": "pays"
+			"streetAddress": "44 Rue Emeriau",
+			"addressLocality": "Paris",
+			"postalCode": "75015",
+			"addressCountry": "France"
 		},
-		"hasMap": "url-map",
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4747926412724!2d2.283332315674047!3d48.84915597928655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e670051bcb9603%3A0x7b154ae8da0ea541!2sFront+de+Seine+-+Squash+%26+Fitness!5e0!3m2!1sfr!2sfr!4v1537218453616",
 		"openingHours": [
-			"Mo-Sa 11:00-14:30",
-			"Mo-Th 17:00-21:30",
-			"Fr-Sa 17:00-22:00"
+			"Mo-Fr 6:45-23:30",
+			"Sa 9:00-20:30",
+			"Su 9:00-20:30"
 		]
 	}
 [ends tags='script' /]
--- publication/consumer/loisirs/sport/front-de-seine.php
+++ publication/consumer/loisirs/sport/front-de-seine.php
@@ -1,13 +1,13 @@
-[img src="https://business.livingxworld.com/images/logo/loisirs/sport/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" /]
+[img src="https://business.livingxworld.com/images/logo/loisirs/sport/frontdeseine.png" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="front de seine" /]
 <hr id="system-readmore" />
 [h2 class="text-center"][begins tags="strong" /]Presentation[ends tags="strong" /] [/h2]
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
-		
+		Sport. True.
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
-		[youtube source="" /]
+		[youtube source="AL44ukHbdPg" /]
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -16,21 +16,21 @@
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[h3]coordinated[/h3]
-		[fa name="external-link" zoom="2x" /] [a href="https://" target="_blank" rel="dofollow"]Website[/a] [br /]
-		[fa name="map-marker-alt" zoom="2x" /] adresse [br /]
-		[fa name="phone" zoom="2x" /] [a href="tel:+" rel="nofollow"][/a] [br /]
-		SIRET/VAT [a href="https://" rel="nofollow"][/a] [br /]
+		[fa name="external-link" zoom="2x" /] [a href="https://frontdeseine.net" target="_blank" rel="dofollow"]Website[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 44 Rue Emeriau, 75015 Paris [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33145753537" rel="nofollow"]01 45 75 35 37[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]--[/a] [br /]
 		[h3]Opening schedules[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /]Monday to Friday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Saturday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Sunday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]holidays: xxhxx - xxhxx[ends tags="li" /]
+			[begins tags="li" /]Monday to Friday: 6h45 - 23h30[ends tags="li" /]
+			[begins tags="li" /]Saturday: 9h00 - 20h30[ends tags="li" /]
+			[begins tags="li" /]Sunday: 9h00 - 20h30[ends tags="li" /]
+			[begins tags="li" /]holidays: --[ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Transport[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /][fa name="bus" zoom="2x" /]  --[ends tags="li" /]
-			[begins tags="li" /][fa name="subway" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="bus" zoom="2x" /] Théâtre: 42 [ends tags="li" /]
+			[begins tags="li" /][fa name="subway" zoom="2x" /] Commerce: 8 / Dupleix: 6 / Charles Michels: 10[ends tags="li" /]
 			[begins tags="li" /][fa name="train" zoom="2x" /]  --[ends tags="li" /]
 			[begins tags="li" /][fa name="car" zoom="2x" /]  --[ends tags="li" /]
 		[ends tags="ul" /]	
@@ -51,7 +51,7 @@
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
 		[h3]MAP[/h3]
-		[gmaps url="" /]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4747926412724!2d2.283332315674047!3d48.84915597928655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e670051bcb9603%3A0x7b154ae8da0ea541!2sFront+de+Seine+-+Squash+%26+Fitness!5e0!3m2!1sfr!2sfr!4v1537218453616" /]
 		[h4]Are you interested? [begins tags="small"]Click on the affiliate banner[ends="small" /][/h4] [br /]
 		
 	[ends tags="div" /]	
@@ -83,13 +83,13 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]owner site[/h3]
-		[whois-domains url="" /] 
+		[whois-domains url="frontdeseine.net" /] 
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]HTTPS: SSL/TLS[/h3]
-		[ssllabs domains="" /]
+		[ssllabs domains="frontdeseine.net" /]
 		[h3]Your data are[/h3]
-		[api-myipms query="" /]	
+		[api-myipms query="frontdeseine.net" /]	
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -101,26 +101,36 @@
 			[begins tags="table" class="table table-bordered" mdataprop="offers" mdatatype="http://schema.org/Offer" /] 
 				[begins tags="thead" /] 
 					[begins tags="tr" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
+						[begins tags="th" /]Type[ends tags="th" /] 
+						[begins tags="th" /]Initial[ends tags="th" /] 
+						[begins tags="th" /]Basic[ends tags="th" /] 
+						[begins tags="th" /]Avantage[ends tags="th" /] 
 					[ends tags="tr" /] 
 				[ends tags="thead" /] 
 				[begins tags="tbody" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-					[ends tags="tr" /]
-					[begins tags="tr" /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags="td" /]Squash[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="29"' /]€29.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
+						[begins tags='td' mdataprop='price' more='content="23"' /]€23.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="17"' /]€17.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
 					[ends tags="tr" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a][ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a] [ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a][ends tags="td" /] 
+						[begins tags="td" /]Training Gym[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="29"' /]€29.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
+						[begins tags='td' mdataprop='price' more='content="25"' /]€25.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="19"' /]€19.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /] 
+						[begins tags="td" /]Spinning[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="29"' /]€29.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
+						[begins tags='td' mdataprop='price' more='content="25"' /]€25.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="19"' /]€19.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /] 
+						[begins tags="td" /]--[ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.frontdeseine.net/acheter/" mdataprop="url"]buy offer[/a][ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.frontdeseine.net/acheter/" mdataprop="url"]buy offer[/a] [ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.frontdeseine.net/acheter/" mdataprop="url"]buy offer[/a][ends tags="td" /] 
 					[ends tags="tr" /]
 				[ends tags="tbody" /] 
 			[ends tags="table" /] 
@@ -133,10 +143,10 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-6" /]
 		[h3]Badge[/h3]
-		[begins tags="div" class="card text-white bg-" /]
-			[lxw-badge parent="loisirs" category="sport" entreprise="" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-" showembed="yes" /]
+		[begins tags="div" class="card text-white bg-warning" /]
+			[lxw-badge parent="loisirs" category="sport" entreprise="front-de-seine" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-warning" showembed="yes" /]
 			[begins tags="div" class="card-body" /]
-				[h5 class="card-title"]advice final[/h5]
+				[h5 class="card-title"]doudt[/h5]
 			[ends tags="div" /]	
 		[ends tags="div" /]	
 	[ends tags="div" /]	
@@ -153,7 +163,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Web Regulations[ends tags="dt" /] 
-				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+				[begins tags="dd" /] incompleted[ends tags="dd" /] 
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Tester reviews[ends tags="dt" /] 
@@ -161,7 +171,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Web security test[ends tags="dt" /] 
-				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+				[begins tags="dd" /] Let's Encrypt Authority X3 : A[ends tags="dd" /] 
 			[ends tags="dl" /]
 		[ends tags="blockquote" /]
 	[ends tags="div" /]	
@@ -182,27 +192,26 @@
 	{
 		"@context": "http://www.schema.org",
 		"@type": "LocalBusiness",
-		"name": "nom",
-		"url": "https://url.com",
-		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/",
-		"telephone": "+33100000000",
-		"description": "description",
+		"name": "front de seine",
+		"url": "https://frontdeseine.net",
+		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/frontdeseine.png",
+		"telephone": "+33145753537",
+		"description": "Sport. True.",
 		"currenciesAccepted": "EUR",
 		"paymentAccepted": "cash, card",
 		"priceRange": "€€",
 		"address": {
 			"@type": "PostalAddress",
-			"streetAddress": "adresse",
-			"addressLocality": "Ville",
-			"addressRegion": "région",
-			"postalCode": "code postal",
-			"addressCountry": "pays"
+			"streetAddress": "44 Rue Emeriau",
+			"addressLocality": "Paris",
+			"postalCode": "75015",
+			"addressCountry": "France"
 		},
-		"hasMap": "url-map",
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4747926412724!2d2.283332315674047!3d48.84915597928655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e670051bcb9603%3A0x7b154ae8da0ea541!2sFront+de+Seine+-+Squash+%26+Fitness!5e0!3m2!1sfr!2sfr!4v1537218453616",
 		"openingHours": [
-			"Mo-Sa 11:00-14:30",
-			"Mo-Th 17:00-21:30",
-			"Fr-Sa 17:00-22:00"
+			"Mo-Fr 6:45-23:30",
+			"Sa 9:00-20:30",
+			"Su 9:00-20:30"
 		]
 	}
 [ends tags='script' /]
