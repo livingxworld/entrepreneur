1055f38ee5c5651b7f22cbf15de2f15af4b01ea1
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2018-09-19 11:48:38 +0200

    add & scan spartiate gym

---
+++ publication/consumer/loisirs/sport/spartiategym.php
@@ -0,0 +1,192 @@
+[img src="https://business.livingxworld.com/images/logo/loisirs/sport/spartiategym.png" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="spartiate gym" /]
+<hr id="system-readmore" />
+[h2 class="text-center"][begins tags="strong" /]Présentation[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
+		salle de musculation et fitness  à La teste de buch
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[youtube source="U0qpFX-Rm6E" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Information[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[h3]Coordonné[/h3]
+		[fa name="external-link" zoom="2x" /] [a href="https://spartiategym.business.site" target="_blank" rel="dofollow"]Site internet[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 23 avenue du général leclerc 33260 la teste de buch France [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33556837937" rel="nofollow"]05 56 83 79 37[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]--[/a] [br /]
+		[h3]Horaires[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /]Lundi à Vendredi: 8h00 - 23h00[ends tags="li" /]
+			[begins tags="li" /]Samedi: 8h00 - 23h00[ends tags="li" /]
+			[begins tags="li" /]Dimanche: 8h00 - 23h00[ends tags="li" /]
+			[begins tags="li" /]Jour férié: --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Transport[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /][fa name="bus" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="subway" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="train" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="car" zoom="2x" /]  --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Autres[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /][fa name="wheelchair" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="broadcast-tower" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="wifi" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="money-bill" zoom="2x" /] --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Ces logiciels et Apps[/h3]
+		[begins tags="ul" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="apple" zoom="2x" /] Apple[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="windows" zoom="2x" /] Microsoft[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="android" zoom="2x" /] Android[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="linux" zoom="2x" /] Linux[/a][ends tags="li" /]
+		[ends tags="ul" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[h3]La carte[/h3]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2838.896579398302!2d-1.1514825844699617!3d44.640030679099844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd549ec7313c5621%3A0x6d14bf13c7b9bbeb!2sSPARTIATEGYM!5e0!3m2!1sfr!2sfr!4v1537348274519" /]
+		[h4]Êtes-vous intéresser? [begins tags="small"]Cliquer sur la bannière d'affiliation[ends="small" /][/h4] [br /]
+		
+	[ends tags="div" /]	
+[ends tags="div" /]
+
+	
+[h2 class="text-center"][begins tags="strong" /]Scanner automatique[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]Priotitaire du site[/h3]
+		-- 
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]HTTPS: SSL/TLS[/h3]
+		--
+		[h3]Vos données sont:[/h3]
+		--
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+[none]
+[h2 class="text-center"][begins tags="strong" /]Ces photo[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt=""" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt=""" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-3" mdatatype="http://schema.org/ImageObject" /]
+		[h4][/h4]
+		[img src="https://business.livingxworld.com/images/photo/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt=""" mdataprop="contentUrl" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+[/none]
+
+
+[h2 class="text-center"][begins tags="strong" /]Les Tarifs Proposés en TTC (sans promotion)[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-12" /]	
+		[begins tags="div" class="table-responsive" /] 
+			[begins tags="table" class="table table-bordered" mdataprop="offers" mdatatype="http://schema.org/Offer" /] 
+				[begins tags="thead" /] 
+					[begins tags="tr" /] 
+						[begins tags="th" /]Yearly[ends tags="th" /] 
+					[ends tags="tr" /] 
+				[ends tags="thead" /] 
+				[begins tags="tbody" /] 
+					[begins tags="tr" /] 
+						[begins tags='td' mdataprop='price' more='content="00"' /]€300.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /] 
+						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a][ends tags="td" /] 
+					[ends tags="tr" /]
+				[ends tags="tbody" /] 
+			[ends tags="table" /] 
+		[ends tags="div" /]	
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+[h2 class="text-center"][begins tags="strong" /]Note et avis avec exigence[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Badge[/h3]
+		[begins tags="div" class="card text-white bg-success" /]
+			[lxw-badge parent="loisirs" category="sport" entreprise="spartiategym" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-success" showembed="yes" /]
+			[begins tags="div" class="card-body" /]
+				[h5 class="card-title"]validé[/h5]
+			[ends tags="div" /]	
+		[ends tags="div" /]		
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Contrôle indépendant[/h3]
+		[begins tags="blockquote" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Services chez le client[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Ambiance d'équipe[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Réglementation web[ends tags="dt" /] 
+				[begins tags="dd" /] conforme[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Avis testeur[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Teste de sécurité web[ends tags="dt" /] 
+				[begins tags="dd" /] Google Internet Authority G3 :A[ends tags="dd" /] 
+			[ends tags="dl" /] 
+		[ends tags="blockquote" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+[h2 class="text-center"][begins tags="strong" /]Faire connaître[ends tags="strong" /] [/h2]
+	[begins tags="div" class="row" /] 
+		[begins tags="div" class="col-md-12 center-block text-center" /]
+			[div class="addthis_inline_share_toolbox" /] 
+		[ends tags="div" /] 
+	[ends tags="div" /]
+ [br /] [br /]
+[h6]A vous de donner votre opignon: selon ses services clientèles, ses proposition, qualité de la prestation et encore plus.[/h6]
+
+
+[begins tags='script' more='type="application/ld+json"' /] 
+	{
+		"@context": "http://www.schema.org",
+		"@type": "LocalBusiness",
+		"name": "spartiate gym",
+		"url": "https://spartiategym.business.site/",
+		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/spartiategym.png",
+		"telephone": "+33556837937",
+		"description": "salle de musculation et fitness  à La teste de buch",
+		"currenciesAccepted": "EUR",
+		"paymentAccepted": "cash, card",
+		"priceRange": "€€",
+		"address": {
+			"@type": "PostalAddress",
+			"streetAddress": "23 avenue du général leclerc",
+			"addressLocality": "La Teste-de-Buch",
+			"postalCode": "33260",
+			"addressCountry": "France"
+		},
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2838.896579398302!2d-1.1514825844699617!3d44.640030679099844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd549ec7313c5621%3A0x6d14bf13c7b9bbeb!2sSPARTIATEGYM!5e0!3m2!1sfr!2sfr!4v1537348274519",
+		"openingHours": [
+			"Mo-Su 8:00-23:00"
+		]
+	}
+[ends tags='script' /]
