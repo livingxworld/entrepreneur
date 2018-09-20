06acf6ca45ff895e98c6d0d0c4cc886846efed84
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2018-09-20 07:00:19 +0200

    scan & update point gym

--- publication/consommateur/loisirs/sport/point-gym.php
+++ publication/consommateur/loisirs/sport/point-gym.php
@@ -1,13 +1,13 @@
-[img src="https://business.livingxworld.com/images/logo/loisirs/sport/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" /]
+[img src="https://business.livingxworld.com/images/logo/loisirs/sport/pointgym.jpg" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="pointgym" /]
 <hr id="system-readmore" />
 [h2 class="text-center"][begins tags="strong" /]Présentation[ends tags="strong" /] [/h2]
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
-		
+		Une salle de gym convivialeDes cours de gym diversifiés (gym douce,gym posturale,stretching,gym du dos,culture physique,pilates,abdos,yoga,qi gong,danse classique,street jazz,zumba, du coaching  et un espace massage où vous pouvez vous détendre à tout moment de la journée. Notre équipe sera présente pour vous motiver, vous conseiller et mesurer vos progrès.N'attendez plus! procurez vous un Pass Découverte et tester notre Point Gym
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
-		[youtube source="" /]
+		L'auteur ne contient pas de vidéo
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -16,22 +16,23 @@
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[h3]Coordonné[/h3]
-		[fa name="external-link" zoom="2x" /] [a href="https://" target="_blank" rel="dofollow"]Site internet[/a] [br /]
-		[fa name="map-marker-alt" zoom="2x" /] adresse [br /]
-		[fa name="phone" zoom="2x" /] [a href="tel:+" rel="nofollow"][/a] [br /]
-		SIRET/VAT [a href="https://" rel="nofollow"][/a] [br /]
+		[fa name="external-link" zoom="2x" /] [a href="https://pointgym.fr" target="_blank" rel="dofollow"]Site internet[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 211Bis Rue de la Croix Nivert, 75015 Paris [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33145337109" rel="nofollow"]01 45 33 71 09[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]--[/a] [br /]
 		[h3]Horaires[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /]Lundi à Vendredi: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Samedi: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Dimanche: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Jour férié: xxhxx - xxhxx[ends tags="li" /]
+			[begins tags="li" /]Lundi à Vendredi: 9h00 - 21h30[ends tags="li" /]
+			[begins tags="li" /]Samedi: 9h00 - 16h00[ends tags="li" /]
+			[begins tags="li" /]Dimanche: Fermer[ends tags="li" /]
+			[begins tags="li" /]Jour férié: --[ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Transport[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /][fa name="bus" zoom="2x" /]  --[ends tags="li" /]
-			[begins tags="li" /][fa name="subway" zoom="2x" /]  --[ends tags="li" /]
-			[begins tags="li" /][fa name="train" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="bus" zoom="2x" /]  32-62-80
+[ends tags="li" /]
+			[begins tags="li" /][fa name="subway" zoom="2x" /]  Convention - Boucicaut - Porte de Versailles[ends tags="li" /]
+			[begins tags="li" /][fa name="train" zoom="2x" /]  Desnouettes[ends tags="li" /]
 			[begins tags="li" /][fa name="car" zoom="2x" /]  --[ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Autres[/h3] [br /]
@@ -51,7 +52,7 @@
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
 		[h3]La carte[/h3]
-		[gmaps url="" /]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.143005470834!2d2.2877576156735935!3d48.83641087928533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6706cda1b329b%3A0x3a2d8fe67ee00fb5!2sPoint+Gym!5e0!3m2!1sfr!2sfr!4v1537418387711" /]
 		[h4]Êtes-vous intéresser? [begins tags="small"]Cliquer sur la bannière d'affiliation[ends="small" /][/h4] [br /]
 		
 	[ends tags="div" /]	
@@ -62,13 +63,13 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]Priotitaire du site[/h3]
-		[whois-domains url="" /] 
+		[whois-domains url="pointgym.fr" /] 
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]HTTPS: SSL/TLS[/h3]
-		[ssllabs domains="" /]
+		[ssllabs domains="pointgym.fr" /]
 		[h3]Vos données sont:[/h3]
-		[api-myipms query="" /]
+		[api-myipms query="pointgym.fr" /]
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -102,26 +103,36 @@
 			[begins tags="table" class="table table-bordered" mdataprop="offers" mdatatype="http://schema.org/Offer" /] 
 				[begins tags="thead" /] 
 					[begins tags="tr" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
+						[begins tags="th" /]Type[ends tags="th" /] 
+						[begins tags="th" /]Année[ends tags="th" /] 
+						[begins tags="th" /]Trimestre[ends tags="th" /] 
 					[ends tags="tr" /] 
 				[ends tags="thead" /] 
 				[begins tags="tbody" /] 
-					[begins tags="tr" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-					[ends tags="tr" /]
-					[begins tags="tr" /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]Club gym[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="805"' /]€805.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="350"' /]€350.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]équilibre[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="825"' /]€825.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="360"' /]€360.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]Evasion[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="999"' /]€999.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="460"' /]€460.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]Club traning[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="600"' /]€600.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="260"' /]€260.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
 					[ends tags="tr" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a] [ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
+						[begins tags="td" /]--[ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.pointgym.fr/tarifs/" mdataprop="url"]buy offer[/a] [ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.pointgym.fr/tarifs/" mdataprop="url"]buy offer[/a][ends tags="td" /] 
 					[ends tags="tr" /]
 				[ends tags="tbody" /] 
 			[ends tags="table" /] 
@@ -133,10 +144,10 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-6" /]
 		[h3]Badge[/h3]
-		[begins tags="div" class="card text-white bg-" /]
-			[lxw-badge parent="loisirs" category="sport" entreprise="" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-" showembed="yes" /]
+		[begins tags="div" class="card text-white bg-warning" /]
+			[lxw-badge parent="loisirs" category="sport" entreprise="pointgym" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-warning" showembed="yes" /]
 			[begins tags="div" class="card-body" /]
-				[h5 class="card-title"]advice final[/h5]
+				[h5 class="card-title"]douteux[/h5]
 			[ends tags="div" /]	
 		[ends tags="div" /]		
 	[ends tags="div" /]	
@@ -153,7 +164,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Réglementation web[ends tags="dt" /] 
-				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+				[begins tags="dd" /] incomplet[ends tags="dd" /] 
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Avis testeur[ends tags="dt" /] 
@@ -161,7 +172,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Teste de sécurité web[ends tags="dt" /] 
-				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+				[begins tags="dd" /] Let's Encrypt Authority X3 : A[ends tags="dd" /] 
 			[ends tags="dl" /] 
 		[ends tags="blockquote" /]
 	[ends tags="div" /]	
@@ -180,27 +191,25 @@
 	{
 		"@context": "http://www.schema.org",
 		"@type": "LocalBusiness",
-		"name": "nom",
-		"url": "https://url.com",
-		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/",
-		"telephone": "+33100000000",
-		"description": "description",
+		"name": "pointgym",
+		"url": "https://pointgym.fr",
+		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/pointgym.jpg",
+		"telephone": "+33145337109",
+		"description": "Une salle de gym convivialeDes cours de gym diversifiés (gym douce,gym posturale,stretching,gym du dos,culture physique,pilates,abdos,yoga,qi gong,danse classique,street jazz,zumba, du coaching  et un espace massage où vous pouvez vous détendre à tout moment de la journée. Notre équipe sera présente pour vous motiver, vous conseiller et mesurer vos progrès.N'attendez plus! procurez vous un Pass Découverte et tester notre Point Gym",
 		"currenciesAccepted": "EUR",
-		"paymentAccepted": "cash, card",
-		"priceRange": "€€",
+		"paymentAccepted": "paypal",
+		"priceRange": "€€€",
 		"address": {
 			"@type": "PostalAddress",
-			"streetAddress": "adresse",
-			"addressLocality": "Ville",
-			"addressRegion": "région",
-			"postalCode": "code postal",
-			"addressCountry": "pays"
+			"streetAddress": "211Bis Rue de la Croix Nivert",
+			"addressLocality": "Paris",
+			"postalCode": "75015",
+			"addressCountry": "France"
 		},
-		"hasMap": "url-map",
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.143005470834!2d2.2877576156735935!3d48.83641087928533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6706cda1b329b%3A0x3a2d8fe67ee00fb5!2sPoint+Gym!5e0!3m2!1sfr!2sfr!4v1537418387711",
 		"openingHours": [
-			"Mo-Sa 11:00-14:30",
-			"Mo-Th 17:00-21:30",
-			"Fr-Sa 17:00-22:00"
+			"Mo-Fr 9:00-21:30",
+			"Sa 9:00-16:30"
 		]
 	}
 [ends tags='script' /]
--- publication/consumer/loisirs/sport/point-gym.php
+++ publication/consumer/loisirs/sport/point-gym.php
@@ -1,13 +1,13 @@
-[img src="https://business.livingxworld.com/images/logo/loisirs/sport/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" /]
+[img src="https://business.livingxworld.com/images/logo/loisirs/sport/pointgym.jpg" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="pointgym" /]
 <hr id="system-readmore" />
 [h2 class="text-center"][begins tags="strong" /]Presentation[ends tags="strong" /] [/h2]
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
-		
+		A friendly gymGreat gym classes (soft gym, postural gym, stretching, back gym, fitness, pilates, abs, yoga, qi gong, ballet, street jazz, zumba, coaching and a massage area where you You can relax at any time of the day.Our team will be there to motivate you, advise you and measure your progress.Do not wait any longer! Get a Discovery Pass and test our Point Gym
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
-		[youtube source="" /]
+		The author does not contain video
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -16,22 +16,23 @@
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[h3]coordinated[/h3]
-		[fa name="external-link" zoom="2x" /] [a href="https://" target="_blank" rel="dofollow"]Website[/a] [br /]
-		[fa name="map-marker-alt" zoom="2x" /] adresse [br /]
-		[fa name="phone" zoom="2x" /] [a href="tel:+" rel="nofollow"][/a] [br /]
-		SIRET/VAT [a href="https://" rel="nofollow"][/a] [br /]
+		[fa name="external-link" zoom="2x" /] [a href="https://pointgym.fr" target="_blank" rel="dofollow"]Website[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 211Bis Rue de la Croix Nivert, 75015 Paris [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33145337109" rel="nofollow"]01 45 33 71 09[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]--[/a] [br /]
 		[h3]Opening schedules[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /]Monday to Friday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Saturday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Sunday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]holidays: xxhxx - xxhxx[ends tags="li" /]
+			[begins tags="li" /]Monday to Friday: 9h00 - 21h30[ends tags="li" /]
+			[begins tags="li" /]Saturday: 9h00 - 16h00[ends tags="li" /]
+			[begins tags="li" /]Sunday: Closed[ends tags="li" /]
+			[begins tags="li" /]holidays: --[ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Transport[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /][fa name="bus" zoom="2x" /]  --[ends tags="li" /]
-			[begins tags="li" /][fa name="subway" zoom="2x" /]  --[ends tags="li" /]
-			[begins tags="li" /][fa name="train" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="bus" zoom="2x" /]  32-62-80
+[ends tags="li" /]
+			[begins tags="li" /][fa name="subway" zoom="2x" /]  Convention - Boucicaut - Porte de Versailles[ends tags="li" /]
+			[begins tags="li" /][fa name="train" zoom="2x" /]  Desnouettes[ends tags="li" /]
 			[begins tags="li" /][fa name="car" zoom="2x" /]  --[ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Others[/h3] [br /]
@@ -51,7 +52,7 @@
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
 		[h3]MAP[/h3]
-		[gmaps url="" /]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.143005470834!2d2.2877576156735935!3d48.83641087928533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6706cda1b329b%3A0x3a2d8fe67ee00fb5!2sPoint+Gym!5e0!3m2!1sfr!2sfr!4v1537418387711" /]
 		[h4]Are you interested? [begins tags="small"]Click on the affiliate banner[ends="small" /][/h4] [br /]
 		
 	[ends tags="div" /]	
@@ -83,13 +84,13 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]owner site[/h3]
-		[whois-domains url="" /] 
+		[whois-domains url="pointgym.fr" /] 
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]HTTPS: SSL/TLS[/h3]
-		[ssllabs domains="" /]
+		[ssllabs domains="pointgym.fr" /]
 		[h3]Your data are[/h3]
-		[api-myipms query="" /]	
+		[api-myipms query="pointgym.fr" /]	
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -101,26 +102,36 @@
 			[begins tags="table" class="table table-bordered" mdataprop="offers" mdatatype="http://schema.org/Offer" /] 
 				[begins tags="thead" /] 
 					[begins tags="tr" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
+						[begins tags="th" /]Type[ends tags="th" /] 
+						[begins tags="th" /]yearly[ends tags="th" /] 
+						[begins tags="th" /]quater[ends tags="th" /] 
 					[ends tags="tr" /] 
 				[ends tags="thead" /] 
 				[begins tags="tbody" /] 
-					[begins tags="tr" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-					[ends tags="tr" /]
-					[begins tags="tr" /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]Club gym[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="805"' /]€805.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="350"' /]€350.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]équilibre[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="825"' /]€825.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="360"' /]€360.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]Evasion[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="999"' /]€999.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="460"' /]€460.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+					[ends tags="tr" /] 
+					[begins tags="tr" /]  
+						[begins tags="td" /]Club traning[ends tags="td" /] 
+						[begins tags='td' mdataprop='price' more='content="600"' /]€600.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="260"' /]€260.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
 					[ends tags="tr" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a][ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a] [ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a][ends tags="td" /] 
+						[begins tags="td" /]--[ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.pointgym.fr/tarifs/" mdataprop="url"]buy offer[/a] [ends tags="td" /] 
+						[begins tags="td" /][a href="https://www.pointgym.fr/tarifs/" mdataprop="url"]buy offer[/a][ends tags="td" /] 
 					[ends tags="tr" /]
 				[ends tags="tbody" /] 
 			[ends tags="table" /] 
@@ -133,10 +144,10 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-6" /]
 		[h3]Badge[/h3]
-		[begins tags="div" class="card text-white bg-" /]
-			[lxw-badge parent="loisirs" category="sport" entreprise="" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-" showembed="yes" /]
+		[begins tags="div" class="card text-white bg-warning" /]
+			[lxw-badge parent="loisirs" category="sport" entreprise="pointgym" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-warning" showembed="yes" /]
 			[begins tags="div" class="card-body" /]
-				[h5 class="card-title"]advice final[/h5]
+				[h5 class="card-title"]doudt[/h5]
 			[ends tags="div" /]	
 		[ends tags="div" /]	
 	[ends tags="div" /]	
@@ -153,7 +164,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Web Regulations[ends tags="dt" /] 
-				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+				[begins tags="dd" /] not completed[ends tags="dd" /] 
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Tester reviews[ends tags="dt" /] 
@@ -161,7 +172,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Web security test[ends tags="dt" /] 
-				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+				[begins tags="dd" /] Let's Encrypt Authority X3 : A[ends tags="dd" /] 
 			[ends tags="dl" /]
 		[ends tags="blockquote" /]
 	[ends tags="div" /]	
@@ -182,27 +193,25 @@
 	{
 		"@context": "http://www.schema.org",
 		"@type": "LocalBusiness",
-		"name": "nom",
-		"url": "https://url.com",
-		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/",
-		"telephone": "+33100000000",
-		"description": "description",
+		"name": "pointgym",
+		"url": "https://pointgym.fr",
+		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/pointgym.jpg",
+		"telephone": "+33145337109",
+		"description": "A friendly gymGreat gym classes (soft gym, postural gym, stretching, back gym, fitness, pilates, abs, yoga, qi gong, ballet, street jazz, zumba, coaching and a massage area where you You can relax at any time of the day.Our team will be there to motivate you, advise you and measure your progress.Do not wait any longer! Get a Discovery Pass and test our Point Gym",
 		"currenciesAccepted": "EUR",
-		"paymentAccepted": "cash, card",
-		"priceRange": "€€",
+		"paymentAccepted": "paypal",
+		"priceRange": "€€€",
 		"address": {
 			"@type": "PostalAddress",
-			"streetAddress": "adresse",
-			"addressLocality": "Ville",
-			"addressRegion": "région",
-			"postalCode": "code postal",
-			"addressCountry": "pays"
+			"streetAddress": "211Bis Rue de la Croix Nivert",
+			"addressLocality": "Paris",
+			"postalCode": "75015",
+			"addressCountry": "France"
 		},
-		"hasMap": "url-map",
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.143005470834!2d2.2877576156735935!3d48.83641087928533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6706cda1b329b%3A0x3a2d8fe67ee00fb5!2sPoint+Gym!5e0!3m2!1sfr!2sfr!4v1537418387711",
 		"openingHours": [
-			"Mo-Sa 11:00-14:30",
-			"Mo-Th 17:00-21:30",
-			"Fr-Sa 17:00-22:00"
+			"Mo-Fr 9:00-21:30",
+			"Sa 9:00-16:30"
 		]
 	}
 [ends tags='script' /]
