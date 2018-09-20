37b941190b71cdc2274b0d62915126fa6b7ead76
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2018-09-20 06:16:10 +0200

    scan & update slimtec concept

--- publication/consommateur/loisirs/sport/slimtec-concept.php
+++ publication/consommateur/loisirs/sport/slimtec-concept.php
@@ -1,13 +1,13 @@
-[img src="https://business.livingxworld.com/images/logo/loisirs/sport/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" /]
+[img src="https://business.livingxworld.com/images/logo/loisirs/sport/slimtec-concept.png" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="slimtec concept" /]
 <hr id="system-readmore" />
 [h2 class="text-center"][begins tags="strong" /]Présentation[ends tags="strong" /] [/h2]
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
-		
+		Bienvenue chez Slimtec Concept à Paris dans le 17ème arrondissement et à Asnières. Centre dédié au bien-être, à la remise en forme et à la beauté
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
-		[youtube source="" /]
+		L'auteur ne contient pas de vidéo de présentation
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -16,16 +16,17 @@
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[h3]Coordonné[/h3]
-		[fa name="external-link" zoom="2x" /] [a href="https://" target="_blank" rel="dofollow"]Site internet[/a] [br /]
-		[fa name="map-marker-alt" zoom="2x" /] adresse [br /]
-		[fa name="phone" zoom="2x" /] [a href="tel:+" rel="nofollow"][/a] [br /]
-		SIRET/VAT [a href="https://" rel="nofollow"][/a] [br /]
+		[fa name="external-link" zoom="2x" /] [a href="https://slimtec-concept.fr" target="_blank" rel="dofollow"]Site internet[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 100 Rue de Tocqueville, 75017 Paris [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33140548376" rel="nofollow"]01 40 54 83 76[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]--[/a] [br /]
 		[h3]Horaires[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /]Lundi à Vendredi: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Samedi: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Dimanche: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Jour férié: xxhxx - xxhxx[ends tags="li" /]
+			[begins tags="li" /]Lundi, Mercredi & Vendredi: 8h30 - 21h00[ends tags="li" /]
+			[begins tags="li" /]Jeudi, Mardi: 10h00 - 21h00[ends tags="li" /]
+			[begins tags="li" /]Samedi: 9h00 - 14h00[ends tags="li" /]
+			[begins tags="li" /]Dimanche: 10h00 - 13h00[ends tags="li" /]
+			[begins tags="li" /]Jour férié: --[ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Transport[/h3] [br /]
 		[begins tags="ul" /]
@@ -51,7 +52,7 @@
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
 		[h3]La carte[/h3]
-		[gmaps url="" /]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.4959480625726!2d2.306444015675371!3d48.88688477929028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbb4c14c54b%3A0xf3f872a415bce3f0!2sSlimTec+Concept!5e0!3m2!1sfr!2sfr!4v1537414866961" /]
 		[h4]Êtes-vous intéresser? [begins tags="small"]Cliquer sur la bannière d'affiliation[ends="small" /][/h4] [br /]
 		
 	[ends tags="div" /]	
@@ -62,13 +63,13 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]Priotitaire du site[/h3]
-		[whois-domains url="" /] 
+		[whois-domains url="slimtec-concept.fr" /] 
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]HTTPS: SSL/TLS[/h3]
-		[ssllabs domains="" /]
+		[ssllabs domains="slimtec-concept.fr" /]
 		[h3]Vos données sont:[/h3]
-		[api-myipms query="" /]
+		[api-myipms query="slimtec-concept.fr" /]
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -102,26 +103,18 @@
 			[begins tags="table" class="table table-bordered" mdataprop="offers" mdatatype="http://schema.org/Offer" /] 
 				[begins tags="thead" /] 
 					[begins tags="tr" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
+						[begins tags="th" /]Adhésion[ends tags="th" /] 
+						[begins tags="th" /]Offre duo[ends tags="th" /] 
 					[ends tags="tr" /] 
 				[ends tags="thead" /] 
 				[begins tags="tbody" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-					[ends tags="tr" /]
-					[begins tags="tr" /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="79"' /]€79.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="129"' /]€129.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
 					[ends tags="tr" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a] [ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
+						[begins tags="td" /][a href="http://www.slimtec-concept.fr/tarifs-offres-speciales.html" mdataprop="url"]acheter l'offre[/a] [ends tags="td" /] 
+						[begins tags="td" /][a href="http://www.slimtec-concept.fr/tarifs-offres-speciales.html" mdataprop="url"]acheter l'offre[/a][ends tags="td" /] 
 					[ends tags="tr" /]
 				[ends tags="tbody" /] 
 			[ends tags="table" /] 
@@ -133,10 +126,10 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-6" /]
 		[h3]Badge[/h3]
-		[begins tags="div" class="card text-white bg-" /]
-			[lxw-badge parent="loisirs" category="sport" entreprise="" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-" showembed="yes" /]
+		[begins tags="div" class="card text-white bg-danger" /]
+			[lxw-badge parent="loisirs" category="sport" entreprise="slimtec-concept" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-danger" showembed="yes" /]
 			[begins tags="div" class="card-body" /]
-				[h5 class="card-title"]advice final[/h5]
+				[h5 class="card-title"]danger[/h5]
 			[ends tags="div" /]	
 		[ends tags="div" /]		
 	[ends tags="div" /]	
@@ -153,7 +146,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Réglementation web[ends tags="dt" /] 
-				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+				[begins tags="dd" /] non conforme[ends tags="dd" /] 
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Avis testeur[ends tags="dt" /] 
@@ -161,7 +154,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Teste de sécurité web[ends tags="dt" /] 
-				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+				[begins tags="dd" /] aucun ssl/tls utilisé[ends tags="dd" /] 
 			[ends tags="dl" /] 
 		[ends tags="blockquote" /]
 	[ends tags="div" /]	
@@ -180,27 +173,28 @@
 	{
 		"@context": "http://www.schema.org",
 		"@type": "LocalBusiness",
-		"name": "nom",
-		"url": "https://url.com",
-		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/",
-		"telephone": "+33100000000",
-		"description": "description",
+		"name": "slimtec concept",
+		"url": "https://slimtec-concept.fr",
+		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/slimtec-concept.png",
+		"telephone": "+33140548376",
+		"description": "Bienvenue chez Slimtec Concept à Paris dans le 17ème arrondissement et à Asnières. Centre dédié au bien-être, à la remise en forme et à la beauté",
 		"currenciesAccepted": "EUR",
 		"paymentAccepted": "cash, card",
 		"priceRange": "€€",
 		"address": {
 			"@type": "PostalAddress",
-			"streetAddress": "adresse",
-			"addressLocality": "Ville",
+			"streetAddress": "100 Rue de Tocqueville",
+			"addressLocality": "Paris",
 			"addressRegion": "région",
-			"postalCode": "code postal",
-			"addressCountry": "pays"
+			"postalCode": "75017",
+			"addressCountry": "France"
 		},
-		"hasMap": "url-map",
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.4959480625726!2d2.306444015675371!3d48.88688477929028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbb4c14c54b%3A0xf3f872a415bce3f0!2sSlimTec+Concept!5e0!3m2!1sfr!2sfr!4v1537414866961",
 		"openingHours": [
-			"Mo-Sa 11:00-14:30",
-			"Mo-Th 17:00-21:30",
-			"Fr-Sa 17:00-22:00"
+			"Mo,We,Fr 8:30-21:00",
+			"Th,Tu 10:00-21:00",
+			"Sa 9:00-14:00",
+			"Su 10:00-13:00"
 		]
 	}
 [ends tags='script' /]
--- publication/consumer/loisirs/sport/slimtec-concept.php
+++ publication/consumer/loisirs/sport/slimtec-concept.php
@@ -1,13 +1,13 @@
-[img src="https://business.livingxworld.com/images/logo/loisirs/sport/" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" /]
+[img src="https://business.livingxworld.com/images/logo/loisirs/sport/slimtec-concept.png" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="slimtec concept" /]
 <hr id="system-readmore" />
 [h2 class="text-center"][begins tags="strong" /]Presentation[ends tags="strong" /] [/h2]
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
-		
+		Welcome to Slimtec Concept in Paris in the 17th arrondissement and in Asnières. Center dedicated to well-being, fitness and beauty
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
-		[youtube source="" /]
+		The author does not contain a video presentation
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -16,16 +16,17 @@
 [begins tags="div" class="row" /]
 	[begins tags="div" class="col col-12 col-md-4" /]
 		[h3]coordinated[/h3]
-		[fa name="external-link" zoom="2x" /] [a href="https://" target="_blank" rel="dofollow"]Website[/a] [br /]
-		[fa name="map-marker-alt" zoom="2x" /] adresse [br /]
-		[fa name="phone" zoom="2x" /] [a href="tel:+" rel="nofollow"][/a] [br /]
-		SIRET/VAT [a href="https://" rel="nofollow"][/a] [br /]
+		[fa name="external-link" zoom="2x" /] [a href="https://slimtec-concept.fr" target="_blank" rel="dofollow"]Website[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 100 Rue de Tocqueville, 75017 Paris [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33140548376" rel="nofollow"]01 40 54 83 76[/a] [br /]
+		SIRET/VAT [a href="#" rel="nofollow"]--[/a] [br /]
 		[h3]Opening schedules[/h3] [br /]
 		[begins tags="ul" /]
-			[begins tags="li" /]Monday to Friday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Saturday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]Sunday: xxhxx - xxhxx[ends tags="li" /]
-			[begins tags="li" /]holidays: xxhxx - xxhxx[ends tags="li" /]
+			[begins tags="li" /] Monday, Wednesday & Friday: 8:30 am - 9:00 pm [ends tags="li" /]
+			[begins tags="li" /] Thursday, Tuesday: 10:00 am - 9:00 pm [ends tags="li" /]
+			[begins tags="li" /] Saturday: 9:00 am - 2:00 pm [ends tags="li" /]
+			[begins tags="li" /] Sunday: 10:00 - 13:00 [ends tags="li" /]
+			[begins tags="li" /] Holiday: - [ends tags="li" /]
 		[ends tags="ul" /]	
 		[h3]Transport[/h3] [br /]
 		[begins tags="ul" /]
@@ -51,7 +52,7 @@
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-8" /]
 		[h3]MAP[/h3]
-		[gmaps url="" /]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.4959480625726!2d2.306444015675371!3d48.88688477929028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbb4c14c54b%3A0xf3f872a415bce3f0!2sSlimTec+Concept!5e0!3m2!1sfr!2sfr!4v1537414866961" /]
 		[h4]Are you interested? [begins tags="small"]Click on the affiliate banner[ends="small" /][/h4] [br /]
 		
 	[ends tags="div" /]	
@@ -83,13 +84,13 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]owner site[/h3]
-		[whois-domains url="" /] 
+		[whois-domains url="slimtec-concept.fr" /] 
 	[ends tags="div" /]	
 	[begins tags="div" class="col col-12 col-md-12" /]
 		[h3]HTTPS: SSL/TLS[/h3]
-		[ssllabs domains="" /]
+		[ssllabs domains="slimtec-concept.fr" /]
 		[h3]Your data are[/h3]
-		[api-myipms query="" /]	
+		[api-myipms query="slimtec-concept.fr" /]	
 	[ends tags="div" /]	
 [ends tags="div" /]	
 
@@ -101,26 +102,18 @@
 			[begins tags="table" class="table table-bordered" mdataprop="offers" mdatatype="http://schema.org/Offer" /] 
 				[begins tags="thead" /] 
 					[begins tags="tr" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
-						[begins tags="th" /][ends tags="th" /] 
+						[begins tags="th" /]membership[ends tags="th" /] 
+						[begins tags="th" /]Offer duo[ends tags="th" /] 
 					[ends tags="tr" /] 
 				[ends tags="thead" /] 
 				[begins tags="tbody" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-						[begins tags="td" /]--[ends tags="td" /] 
-					[ends tags="tr" /]
-					[begins tags="tr" /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /]  
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
-						[begins tags='td' mdataprop='price' more='content="00"' /]€00.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="79"' /]€79.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
+						[begins tags='td' mdataprop='price' more='content="129"' /]€129.00 [begins tags="span" mdataprop="priceCurrency" /]EUR[ends tags="span" /][ends tags='td' /] 
 					[ends tags="tr" /] 
 					[begins tags="tr" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a][ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a] [ends tags="td" /] 
-						[begins tags="td" /][a href="#" mdataprop="url"]buy offer[/a][ends tags="td" /] 
+						[begins tags="td" /][a href="http://www.slimtec-concept.fr/tarifs-offres-speciales.html" mdataprop="url"]buy offer[/a] [ends tags="td" /] 
+						[begins tags="td" /][a href="http://www.slimtec-concept.fr/tarifs-offres-speciales.html" mdataprop="url"]buy offer[/a][ends tags="td" /] 
 					[ends tags="tr" /]
 				[ends tags="tbody" /] 
 			[ends tags="table" /] 
@@ -133,10 +126,10 @@
 [begins tags="div" class="row text-center" /]
 	[begins tags="div" class="col col-12 col-md-6" /]
 		[h3]Badge[/h3]
-		[begins tags="div" class="card text-white bg-" /]
-			[lxw-badge parent="loisirs" category="sport" entreprise="" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-" showembed="yes" /]
+		[begins tags="div" class="card text-white bg-danger" /]
+			[lxw-badge parent="loisirs" category="sport" entreprise="slimtec-concept" css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-danger" showembed="yes" /]
 			[begins tags="div" class="card-body" /]
-				[h5 class="card-title"]advice final[/h5]
+				[h5 class="card-title"]danger[/h5]
 			[ends tags="div" /]	
 		[ends tags="div" /]	
 	[ends tags="div" /]	
@@ -153,7 +146,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Web Regulations[ends tags="dt" /] 
-				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+				[begins tags="dd" /] not conform[ends tags="dd" /] 
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Tester reviews[ends tags="dt" /] 
@@ -161,7 +154,7 @@
 			[ends tags="dl" /] 
 			[begins tags="dl" /] 
 				[begins tags="dt" /]Web security test[ends tags="dt" /] 
-				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+				[begins tags="dd" /] nothing ssl/tls used[ends tags="dd" /] 
 			[ends tags="dl" /]
 		[ends tags="blockquote" /]
 	[ends tags="div" /]	
@@ -182,27 +175,28 @@
 	{
 		"@context": "http://www.schema.org",
 		"@type": "LocalBusiness",
-		"name": "nom",
-		"url": "https://url.com",
-		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/",
-		"telephone": "+33100000000",
-		"description": "description",
+		"name": "slimtec concept",
+		"url": "https://slimtec-concept.fr",
+		"logo": "https://business.livingxworld.com/images/logo/loisirs/sport/slimtec-concept.png",
+		"telephone": "+33140548376",
+		"description": "Welcome to Slimtec Concept in Paris in the 17th arrondissement and in Asnières. Center dedicated to well-being, fitness and beauty",
 		"currenciesAccepted": "EUR",
 		"paymentAccepted": "cash, card",
 		"priceRange": "€€",
 		"address": {
 			"@type": "PostalAddress",
-			"streetAddress": "adresse",
-			"addressLocality": "Ville",
+			"streetAddress": "100 Rue de Tocqueville",
+			"addressLocality": "Paris",
 			"addressRegion": "région",
-			"postalCode": "code postal",
-			"addressCountry": "pays"
+			"postalCode": "75017",
+			"addressCountry": "France"
 		},
-		"hasMap": "url-map",
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.4959480625726!2d2.306444015675371!3d48.88688477929028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbb4c14c54b%3A0xf3f872a415bce3f0!2sSlimTec+Concept!5e0!3m2!1sfr!2sfr!4v1537414866961",
 		"openingHours": [
-			"Mo-Sa 11:00-14:30",
-			"Mo-Th 17:00-21:30",
-			"Fr-Sa 17:00-22:00"
+			"Mo,We,Fr 8:30-21:00",
+			"Th,Tu 10:00-21:00",
+			"Sa 9:00-14:00",
+			"Su 10:00-13:00"
 		]
 	}
 [ends tags='script' /]
