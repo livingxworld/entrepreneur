04b5b701d624db546d9e29fa18cd56f3f0cd9cfe
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2019-06-18 14:22:12 +0200

    news bank

---
+++ publication/consumer/administration/banque/blochaine.php
@@ -0,0 +1,139 @@
+[img src="https://business.livingx.world/images/logo/administration/banque/blockchaine.png" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="" /]
+<hr id="system-readmore" />
+[h2 class="text-center"][begins tags="strong" /]Presentation[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
+		Blockchain is the world&#x27;s most trusted all-in-one crypto company. We&#x27;re connecting the world to the future of finance through our suite of products including the leading crypto wallet, bitcoin explorer, and market information.
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		The author does not contain video
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Information[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[h3]coordinated[/h3]
+		[fa name="external-link" zoom="2x" /] [a href="https://blockchain.com" target="_blank" rel="dofollow"]Website[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] -- [br /]
+		[fa name="phone" zoom="2x" /] [a href="#"]--[/a] [br /]
+		SIRET/VAT [a href="#"]--[/a] [br /]
+		[h3]Opening schedules[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /]Monday to Friday: --[ends tags="li" /]
+			[begins tags="li" /]Saturday: --[ends tags="li" /]
+			[begins tags="li" /]Sunday: --[ends tags="li" /]
+			[begins tags="li" /]holidays: --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Transport[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /][fa name="bus" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="subway" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="train" zoom="2x" /]  --[ends tags="li" /]
+			[begins tags="li" /][fa name="car" zoom="2x" /]  --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Others[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /][fa name="wheelchair" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="broadcast-tower" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="wifi" zoom="2x" /] --[ends tags="li" /]
+			[begins tags="li" /][fa name="money-bill" zoom="2x" /] --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]These software and Apps[/h3]
+		[begins tags="ul" /]
+			[begins tags="li" /][a href="https://apps.apple.com/fr/developer/blockchain/id838387371" target="_blank"][fa bs="fab" name="apple" zoom="2x" /] Apple[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="windows" zoom="2x" /] Microsoft[/a][ends tags="li" /]
+			[begins tags="li" /][a href="https://play.google.com/store/apps/developer?id=Blockchain+Luxembourg+S.A." target="_blank"][fa bs="fab" name="android" zoom="2x" /] Android[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="linux" zoom="2x" /] Linux[/a][ends tags="li" /]
+		[ends tags="ul" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[h3]MAP[/h3]
+		--
+		[h4]Are you interested? [begins tags="small"]Click on the affiliate banner[ends="small" /][/h4] [br /]
+		
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Automatic scanner[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]owner site[/h3]
+		[whois-domains url="blockchain.com" /] 
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]HTTPS: SSL/TLS[/h3]
+		[ssllabs domains="blockchain.com" /]
+		[h3]Your data are[/h3]
+		[api-myipms query="blockchain.com" /]	
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+
+
+[h2 class="text-center"][begins tags="strong" /]Note and advice with requirement[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Badge[/h3]
+		[begins tags="div" class="card text-white bg-success" /]
+			[lxw-badge css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-success" showembed="yes" /]
+			[begins tags="div" class="card-body" /]
+				[h5 class="card-title"]trust[/h5]
+			[ends tags="div" /]	
+		[ends tags="div" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Independent control[/h3]
+		[begins tags="blockquote" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Customer services[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Team atmosphere[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Web Regulations[ends tags="dt" /] 
+				[begins tags="dd" /] conform[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Tester reviews[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Web security test[ends tags="dt" /] 
+				[begins tags="dd" /] impossible to scan[ends tags="dd" /] 
+			[ends tags="dl" /]
+		[ends tags="blockquote" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Tell a Friends[ends tags="strong" /] [/h2]
+	[begins tags="div" class="row" /] 
+		[begins tags="div" class="col-md-12 center-block text-center" /]
+			[div class="addthis_inline_share_toolbox" /] 
+		[ends tags="div" /] 
+	[ends tags="div" /]
+ [br /] [br /]
+[h6]It is up to you to give your opinion: according to its customer service, its proposals, quality of the service and more.[/h6]
+
+
+[begins tags='script' more='type="application/ld+json"' /] 
+	{
+		"@context": "http://www.schema.org",
+		"@type": "LocalBusiness",
+		"name": "blockchain",
+		"url": "https://blockchain.com",
+		"logo": "https://business.livingx.world/images/logo/administration/banque/blockchaine.png",
+		"description": "Blockchain is the world&#x27;s most trusted all-in-one crypto company. We&#x27;re connecting the world to the future of finance through our suite of products including the leading crypto wallet, bitcoin explorer, and market information.",
+		"currenciesAccepted": "EUR",
+		"paymentAccepted": "cash",
+		"priceRange": "€"
+	}
+[ends tags='script' /]
