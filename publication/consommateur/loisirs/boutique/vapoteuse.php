d92d3544ce5fa07ef095cf58b66dbad08a3e49a6
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2019-07-02 18:51:52 +0200

    news market soon publish

---
+++ publication/consommateur/loisirs/boutique/vapoteuse.php
@@ -0,0 +1,132 @@
+[img src="https://business.livingx.world/images/logo/loisirs/boutique/franprix.jpg" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="franprix" /]
+<hr id="system-readmore" />
+[h2 class="text-center"][begins tags="strong" /]Présentation[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
+		Retrouvez toute l'actualité de Franprix : nos promotions, nos marques, nos services et toutes les infos de nos magasins près de chez vous
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[youtube source="X7V98GqdZFM" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Information[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[h3]Coordonné[/h3]
+		[fa name="external-link" zoom="2x" /] [a href="https://franprix.fr" target="_blank" rel="dofollow"]Site internet[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] 2 RTE DU PLESSIS 94430 CHENNEVIERES SUR MARNE France [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33800350000" rel="nofollow"]0 800 35 00 00[/a] [br /]
+		SIRET/VAT [a href="https://www.societe.com/societe/distribution-franprix-414265165.html" rel="nofollow"]414 265 165[/a] [br /]
+		[h3]Horaires[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /]Lundi à Samedi: 8h30 - 21h00[ends tags="li" /]
+			[begins tags="li" /]Dimanche: 8h30 - 13h00[ends tags="li" /]
+			[begins tags="li" /]Jour férié: --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Ces logiciels et Apps[/h3]
+		[begins tags="ul" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="apple" zoom="2x" /] Apple[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="windows" zoom="2x" /] Microsoft[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="android" zoom="2x" /] Android[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="linux" zoom="2x" /] Linux[/a][ends tags="li" /]
+		[ends tags="ul" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[h3]La carte[/h3]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d10489.72223651226!2d2.314909048526563!3d48.90718858735057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sfranprix!5e0!3m2!1sfr!2sfr!4v1537992674526" /]
+		[h4]Êtes-vous intéresser? [begins tags="small"]Cliquer sur la bannière d'affiliation[ends="small" /][/h4] [br /]
+		[a href="https://www.awin1.com/cread.php?s=2456706&v=13099&q=370173&r=510459"][img src="https://www.awin1.com/cshow.php?s=2456706&v=13099&q=370173&r=510459" /][/a]
+	[ends tags="div" /]	
+[ends tags="div" /]
+
+	
+[h2 class="text-center"][begins tags="strong" /]Scanner automatique[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]Priotitaire du site[/h3]
+		[whois-domains url="franprix.fr" /] 
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]HTTPS: SSL/TLS[/h3]
+		[ssllabs domains="franprix.fr" /]
+		[h3]Vos données sont:[/h3]
+		[api-myipms query="franprix.fr" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Note et avis avec exigence[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Badge[/h3]
+		[begins tags="div" class="card text-white bg-warning" /]
+			[lxw-badge css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-warning" showembed="yes" /]
+			[begins tags="div" class="card-body" /]
+				[h5 class="card-title"]Douteux[/h5]
+			[ends tags="div" /]	
+		[ends tags="div" /]		
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Contrôle indépendant[/h3]
+		[begins tags="blockquote" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Services chez le client[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Ambiance d'équipe[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Réglementation web[ends tags="dt" /] 
+				[begins tags="dd" /] conforme[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Avis testeur[ends tags="dt" /] 
+				[begins tags="dd" /]validé[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Teste de sécurité web[ends tags="dt" /] 
+				[begins tags="dd" /] Symantec Class 3 Secure Server CA - G4 : T[ends tags="dd" /] 
+			[ends tags="dl" /] 
+		[ends tags="blockquote" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+[h2 class="text-center"][begins tags="strong" /]Faire connaître[ends tags="strong" /] [/h2]
+	[begins tags="div" class="row" /] 
+		[begins tags="div" class="col-md-12 center-block text-center" /]
+			[div class="addthis_inline_share_toolbox" /] 
+		[ends tags="div" /] 
+	[ends tags="div" /]
+ [br /] [br /]
+[h6]A vous de donner votre opignon: selon ses services clientèles, ses proposition, qualité de la prestation et encore plus.[/h6]
+
+
+[begins tags='script' more='type="application/ld+json"' /] 
+	{
+		"@context": "http://www.schema.org",
+		"@type": "LocalBusiness",
+		"name": "franprix",
+		"url": "https://franprix.fr",
+		"logo": "https://business.livingx.world/images/logo/loisirs/boutique/franprix.jpg",
+		"telephone": "+33800350000",
+		"description": "Retrouvez toute l'actualité de Franprix : nos promotions, nos marques, nos services et toutes les infos de nos magasins près de chez vous",
+		"currenciesAccepted": "EUR",
+		"paymentAccepted": "cash, card",
+		"address": {
+			"@type": "PostalAddress",
+			"streetAddress": "2 RTE DU PLESSIS",
+			"addressLocality": "CHENNEVIERES SUR MARNE",
+			"postalCode": "94430",
+			"addressCountry": "France"
+		},
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d10489.72223651226!2d2.314909048526563!3d48.90718858735057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sfranprix!5e0!3m2!1sfr!2sfr!4v1537992674526",
+		"openingHours": [
+			"Mo-Sa 8:30-21:00",
+			"Su 8:30-13:00"
+		]
+	}
+[ends tags='script' /]
