48ab4d323b8e8f91308b1db27f051fdafb9405c1
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2019-08-01 14:32:39 +0200

    prepare new market

---
+++ publication/consommateur/loisirs/boutique/news/fitnessboutique.php
@@ -0,0 +1,132 @@
+
+[img src="https://business.livingx.world/images/logo/loisirs/boutique/auchan.jpg" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="auchan" /]
+<hr id="system-readmore" />
+[h2 class="text-center"][begins tags="strong" /]Présentation[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
+		Un choix énorme de produits au meilleur prix, livrables en magasin, en point relais ou à domicile partout en France. Infos sur nos magasins et hypermarchés.
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[youtube source="ZOnrAuHomy8" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Information[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[h3]Coordonné[/h3]
+		[fa name="external-link" zoom="2x" /] [a href="https://auchan.fr" target="_blank" rel="dofollow"]Site internet[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] RUE MAL DE LATTRE DE TASSIGNY 59170 CROIX France [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33359305930" rel="nofollow"]03 5930 5930[/a] [br /]
+		SIRET/VAT [a href="https://www.societe.com/societe/auchan-e-commerce-france-413176033.html" rel="nofollow"]413 176 033[/a] [br /]
+		[h3]Horaires[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /]Lundi à Samedi: 7h30 - 22h00[ends tags="li" /]
+			[begins tags="li" /]Dimanche: 9h00 - 13h00[ends tags="li" /]
+			[begins tags="li" /]Jour férié: --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]Ces logiciels et Apps[/h3]
+		[begins tags="ul" /]
+			[begins tags="li" /][a href="https://itunes.apple.com/fr/developer/auchan/id342888608?mt=8" target="_blank"][fa bs="fab" name="apple" zoom="2x" /] Apple[/a][ends tags="li" /]
+			[begins tags="li" /][a href="https://www.microsoft.com/fr-fr/p/myauchanphone/9nblggh08pj1" target="_blank"][fa bs="fab" name="windows" zoom="2x" /] Microsoft[/a][ends tags="li" /]
+			[begins tags="li" /][a href="https://play.google.com/store/apps/developer?id=Auchan" target="_blank"][fa bs="fab" name="android" zoom="2x" /] Android[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="linux" zoom="2x" /] Linux[/a][ends tags="li" /]
+		[ends tags="ul" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[h3]La carte[/h3]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d41958.8931192752!2d2.2886444350059834!3d48.90718361737622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sauchan!5e0!3m2!1sfr!2sfr!4v1538038651888" /]
+		[h4]Êtes-vous intéresser? [begins tags="small"]Cliquer sur la bannière d'affiliation[ends="small" /][/h4] [br /]
+		
+	[ends tags="div" /]	
+[ends tags="div" /]
+
+	
+[h2 class="text-center"][begins tags="strong" /]Scanner automatique[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]Priotitaire du site[/h3]
+		[whois-domains url="auchan.fr" /] 
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]HTTPS: SSL/TLS[/h3]
+		[ssllabs domains="auchan.fr" /]
+		[h3]Vos données sont:[/h3]
+		[api-myipms query="auchan.fr" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+[h2 class="text-center"][begins tags="strong" /]Note et avis avec exigence[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Badge[/h3]
+		[begins tags="div" class="card text-white bg-success" /]
+			[lxw-badge css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-success" showembed="yes" /]
+			[begins tags="div" class="card-body" /]
+				[h5 class="card-title"]validé[/h5]
+			[ends tags="div" /]	
+		[ends tags="div" /]		
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Contrôle indépendant[/h3]
+		[begins tags="blockquote" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Services chez le client[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Ambiance d'équipe[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Réglementation web[ends tags="dt" /] 
+				[begins tags="dd" /] conforme[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Avis testeur[ends tags="dt" /] 
+				[begins tags="dd" /] en cours...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Teste de sécurité web[ends tags="dt" /] 
+				[begins tags="dd" /] COMODO RSA Extended Validation Secure Server CA : A[ends tags="dd" /] 
+			[ends tags="dl" /] 
+		[ends tags="blockquote" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+[h2 class="text-center"][begins tags="strong" /]Faire connaître[ends tags="strong" /] [/h2]
+	[begins tags="div" class="row" /] 
+		[begins tags="div" class="col-md-12 center-block text-center" /]
+			[div class="addthis_inline_share_toolbox" /] 
+		[ends tags="div" /] 
+	[ends tags="div" /]
+ [br /] [br /]
+[h6]A vous de donner votre opignon: selon ses services clientèles, ses proposition, qualité de la prestation et encore plus.[/h6]
+
+
+[begins tags='script' more='type="application/ld+json"' /] 
+	{
+		"@context": "http://www.schema.org",
+		"@type": "LocalBusiness",
+		"name": "auchan",
+		"url": "https://auchan.fr",
+		"logo": "https://business.livingx.world/images/logo/loisirs/boutique/auchan.jpg",
+		"telephone": "+33359305930",
+		"description": "Un choix énorme de produits au meilleur prix, livrables en magasin, en point relais ou à domicile partout en France. Infos sur nos magasins et hypermarchés.",
+		"currenciesAccepted": "EUR",
+		"paymentAccepted": "cash, card",
+		"address": {
+			"@type": "PostalAddress",
+			"streetAddress": "RUE MAL DE LATTRE DE TASSIGNY",
+			"addressLocality": "CROIX",
+			"postalCode": "59170",
+			"addressCountry": "France"
+		},
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d41958.8931192752!2d2.2886444350059834!3d48.90718361737622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sauchan!5e0!3m2!1sfr!2sfr!4v1538038651888",
+		"openingHours": [
+			"Mo-Sa 7:30-22:00",
+			"Su 9:00-13:00"
+		]
+	}
+[ends tags='script' /]
---
+++ publication/consumer/loisirs/boutique/news/decathlon.php
@@ -0,0 +1,133 @@
+[img src="https://business.livingx.world/images/logo/loisirs/boutique/auchan.jpg" class="img-fluid rounded mx-auto d-block img-thumbnail border border-info" alt="auchan" /]
+<hr id="system-readmore" />
+[h2 class="text-center"][begins tags="strong" /]Presentation[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[begins tags="strong" /]Description:[ends tags="strong" /][br /]
+		A huge selection of products at the best price, available in store, relay point or home anywhere in France. Info on our stores and hypermarkets.
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[youtube source="ZOnrAuHomy8" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Information[ends tags="strong" /] [/h2]
+[begins tags="div" class="row" /]
+	[begins tags="div" class="col col-12 col-md-4" /]
+		[h3]coordinated[/h3]
+		[fa name="external-link" zoom="2x" /] [a href="https://auchan.fr" target="_blank" rel="dofollow"]Website[/a] [br /]
+		[fa name="map-marker-alt" zoom="2x" /] RUE MAL DE LATTRE DE TASSIGNY 59170 CROIX France [br /]
+		[fa name="phone" zoom="2x" /] [a href="tel:+33359305930" rel="nofollow"]03 5930 5930[/a] [br /]
+		SIRET/VAT [a href="https://www.societe.com/societe/auchan-e-commerce-france-413176033.html" rel="nofollow"]413 176 033[/a] [br /]
+		[h3]Opening schedules[/h3] [br /]
+		[begins tags="ul" /]
+			[begins tags="li" /]Monday to Saturday: 7h30 - 22h00[ends tags="li" /]
+			[begins tags="li" /]Sunday: 9h00 - 13h00[ends tags="li" /]
+			[begins tags="li" /]holidays: --[ends tags="li" /]
+		[ends tags="ul" /]	
+		[h3]These software and Apps[/h3]
+		[begins tags="ul" /]
+			[begins tags="li" /][a href="https://itunes.apple.com/fr/developer/auchan/id342888608?mt=8" target="_blank"][fa bs="fab" name="apple" zoom="2x" /] Apple[/a][ends tags="li" /]
+			[begins tags="li" /][a href="https://www.microsoft.com/fr-fr/p/myauchanphone/9nblggh08pj1" target="_blank"][fa bs="fab" name="windows" zoom="2x" /] Microsoft[/a][ends tags="li" /]
+			[begins tags="li" /][a href="https://play.google.com/store/apps/developer?id=Auchan" target="_blank"][fa bs="fab" name="android" zoom="2x" /] Android[/a][ends tags="li" /]
+			[begins tags="li" /][a href="#" target="_blank"][fa bs="fab" name="linux" zoom="2x" /] Linux[/a][ends tags="li" /]
+		[ends tags="ul" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-8" /]
+		[h3]MAP[/h3]
+		[gmaps url="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d41958.8931192752!2d2.2886444350059834!3d48.90718361737622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sauchan!5e0!3m2!1sfr!2sfr!4v1538038651888" /]
+		[h4]Are you interested? [begins tags="small"]Click on the affiliate banner[ends="small" /][/h4] [br /]
+		
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+[h2 class="text-center"][begins tags="strong" /]Automatic scanner[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]owner site[/h3]
+		[whois-domains url="auchan.fr" /] 
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-12" /]
+		[h3]HTTPS: SSL/TLS[/h3]
+		[ssllabs domains="auchan.fr" /]
+		[h3]Your data are[/h3]
+		[api-myipms query="auchan.fr" /]	
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Note and advice with requirement[ends tags="strong" /] [/h2]
+[begins tags="div" class="row text-center" /]
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Badge[/h3]
+		[begins tags="div" class="card text-white bg-success" /]
+			[lxw-badge css="card-img-top img-fluid rounded mx-auto d-block img-thumbnail border border-success" showembed="yes" /]
+			[begins tags="div" class="card-body" /]
+				[h5 class="card-title"]trust[/h5]
+			[ends tags="div" /]	
+		[ends tags="div" /]	
+	[ends tags="div" /]	
+	[begins tags="div" class="col col-12 col-md-6" /]
+		[h3]Independent control[/h3]
+		[begins tags="blockquote" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Customer services[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Team atmosphere[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Web Regulations[ends tags="dt" /] 
+				[begins tags="dd" /]conform[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Tester reviews[ends tags="dt" /] 
+				[begins tags="dd" /] pending ...[ends tags="dd" /] 
+			[ends tags="dl" /] 
+			[begins tags="dl" /] 
+				[begins tags="dt" /]Web security test[ends tags="dt" /] 
+				[begins tags="dd" /] COMODO RSA Extended Validation Secure Server CA : A[ends tags="dd" /] 
+			[ends tags="dl" /]
+		[ends tags="blockquote" /]
+	[ends tags="div" /]	
+[ends tags="div" /]	
+
+
+[h2 class="text-center"][begins tags="strong" /]Tell a Friends[ends tags="strong" /] [/h2]
+	[begins tags="div" class="row" /] 
+		[begins tags="div" class="col-md-12 center-block text-center" /]
+			[div class="addthis_inline_share_toolbox" /] 
+		[ends tags="div" /] 
+	[ends tags="div" /]
+ [br /] [br /]
+[h6]It is up to you to give your opinion: according to its customer service, its proposals, quality of the service and more.[/h6]
+
+
+[begins tags='script' more='type="application/ld+json"' /] 
+	{
+		"@context": "http://www.schema.org",
+		"@type": "LocalBusiness",
+		"name": "auchan",
+		"url": "https://auchan.fr",
+		"logo": "https://business.livingx.world/images/logo/loisirs/boutique/auchan.jpg",
+		"telephone": "+33359305930",
+		"description": "A huge selection of products at the best price, available in store, relay point or home anywhere in France. Info on our stores and hypermarkets.",
+		"currenciesAccepted": "EUR",
+		"paymentAccepted": "cash, card",
+		"address": {
+			"@type": "PostalAddress",
+			"streetAddress": "RUE MAL DE LATTRE DE TASSIGNY",
+			"addressLocality": "CROIX",
+			"postalCode": "59170",
+			"addressCountry": "France"
+		},
+		"hasMap": "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d41958.8931192752!2d2.2886444350059834!3d48.90718361737622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sauchan!5e0!3m2!1sfr!2sfr!4v1538038651888",
+		"openingHours": [
+			"Mo-Sa 7:30-22:00",
+			"Su 9:00-13:00"
+		]
+	}
+[ends tags='script' /]
